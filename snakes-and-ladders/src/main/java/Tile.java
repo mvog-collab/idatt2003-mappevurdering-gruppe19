222ee33383c6b1cba3838fe6cb8db55e13d6a614
Edvard Klavenes <edvardklavenes@gmail.com>
2025-02-12 13:49:14 +0100

    Start: tile class with methods

--- snakes-and-ladders/src/main/java/Tile.java
+++ snakes-and-ladders/src/main/java/Tile.java
@@ -1,3 +1,28 @@
 public class Tile {
 
+    private final int tileId;
+    private final Tile nextTile;
+
+    public Tile(int tileId) {
+        this.tileId = tileId;
+        this.nextTile = new Tile(tileId + 1);
+    }
+
+    public void landPlayer(Player player) {
+
+    }
+
+    public void leavePlayer(Player player) {
+
+    }
+
+    public void setNextTile(Tile nextTile) {
+
+    }
+
+    public int getTileId() {
+        return tileId;
+    }
+
+
 }
