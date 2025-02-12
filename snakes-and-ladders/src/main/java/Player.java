bd81acd32640d8a751b80e333209f0d6e5cb1d1a
Edvard Klavenes <edvardklavenes@gmail.com>
2025-02-12 14:39:51 +0100

    Feat: create player class and methods

--- snakes-and-ladders/src/main/java/Player.java
+++ snakes-and-ladders/src/main/java/Player.java
@@ -1,3 +1,33 @@
 public class Player {
 
+    private final String name;
+    private Tile currentTile;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void placeOnTile(Tile tile) {
+        if (currentTile != null) {
+            currentTile.removePlayerFromTile(this);
+        }
+        currentTile = tile;
+        currentTile.landPlayer(this);
+    }
+
+    public void move(int steps) {
+        Tile targetTile = currentTile;
+        for (int i = 0; i < steps && targetTile.getNextTile() != null; i++) {
+            targetTile = targetTile.getNextTile();
+        }
+        placeOnTile(targetTile);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Tile getCurrentTile() {
+        return currentTile;
+    }
 }
