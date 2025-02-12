2a74de18edf8ca7df80730a1775cec458b9a8350
Martha Luise Vogel <marthavogel04@gmail.com>
2025-02-12 11:14:18 +0100

    Create: dice constructor

--- snakes-and-ladders/src/main/java/Dice.java
+++ snakes-and-ladders/src/main/java/Dice.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Dice {
+
+  private final ArrayList<Die> dice;
+
+  public Dice() {
+    this.dice = new ArrayList<>();
+    Die firstDie = new Die();
+    Die secondDie = new Die();
+
+    this.dice.add(firstDie);
+    this.dice.add(secondDie);
+  }
+
 
 }
