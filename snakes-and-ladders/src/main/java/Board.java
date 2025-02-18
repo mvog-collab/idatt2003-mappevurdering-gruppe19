06712f9dbf21ed11fcb8cc514298541cadde7959
Edvard Klavenes <edvardklavenes@gmail.com>
2025-02-18 16:22:54 +0100

    Add main method for testing and change helper methods

--- snakes-and-ladders/src/main/java/Board.java
+++ snakes-and-ladders/src/main/java/Board.java
@@ -14,9 +14,16 @@ public class Board {
       throw new IllegalArgumentException("Tile cannot be null");
     }
     tiles.put(tile.getTileId(), tile);
+
+    if (tile.getTileId() > 0) {
+      Tile previousTile = tiles.get(tile.getTileId() - 1);
+      if (previousTile != null) {
+        previousTile.setNextTile(tile);
+      }
+    }
   }
 
   public Tile getTile(int tileId) {
-    return tiles.getOrDefault(tileId, null);
+    return tiles.get(tileId);
   }
 }
