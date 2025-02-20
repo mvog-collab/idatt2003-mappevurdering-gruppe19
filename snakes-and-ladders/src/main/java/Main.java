cca6d01d90e4c1cce09d4020cc64ba5ee6dbb0fe
Edvard Klavenes <edvardklavenes@gmail.com>
2025-02-20 10:52:58 +0100

    Update: player object

--- snakes-and-ladders/src/main/java/Main.java
+++ snakes-and-ladders/src/main/java/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void initialize() {
         BoardGame game = new BoardGame();
-        Player Ingrid = new Player("Ingrid");
+        Player Player = new Player("Martha");
 
-        game.addPlayer(Ingrid);
+        game.addPlayer(Player);
         game.createBoard(91);
-        game.setCurrentPlayer(Ingrid);
+        game.setCurrentPlayer(Player);
 
         for (int i = 0; i < 100; i++) {
             game.play();
