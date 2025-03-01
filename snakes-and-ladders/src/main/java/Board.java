ebaa68eaa8d99b28de30fe46156728c7a27373bc
Edvard Klavenes <edvardklavenes@gmail.com>
2025-03-01 14:31:32 +0100

    Setup: snake and ladders configuation

--- snakes-and-ladders/src/main/java/Board.java
+++ snakes-and-ladders/src/main/java/Board.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Board {
 
   private final Map<Integer, Tile> tiles;
+  private final int size;
 
-  public Board() {
+  public Board(int size) {
+    this.size = size;
     this.tiles = new HashMap<>();
+    generateSnakesAndLadders();
   }
 
   public void addTile(Tile tile) {
@@ -21,6 +26,32 @@ public class Board {
         previousTile.setNextTile(tile);
       }
     }
+  }
+
+  private List<TileAction> generateSnakesAndLadders() {
+    List<TileAction> actions = new ArrayList<>();
+
+    if (size > 100) {
+    }
+    actions.add(new Snake(14, 7, this));
+    actions.add(new Snake(48, 26, this));
+    actions.add(new Snake(84, 58, this));
+    actions.add(new Snake(24, 3, this));
+    actions.add(new Snake(45, 15, this));
+
+    actions.add(new Ladder(4, 14, this));
+    actions.add(new Ladder(9, 31, this));
+    actions.add(new Ladder(20, 38, this));
+    actions.add(new Ladder(28, 84, this));
+    actions.add(new Ladder(40, 59, this));
+
+    for (TileAction action : actions) {
+      Tile startTile = getTile(action.getActionPosition());
+      if (startTile != null) {
+          startTile.setAction(action);
+      }
+  }
+  return actions;
   }
 
   public Tile getTile(int tileId) {
