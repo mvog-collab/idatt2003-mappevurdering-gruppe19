1d70808977897f43af0cca4cac919dfa78e8347e
Edvard Klavenes <edvardklavenes@gmail.com>
2025-03-01 14:32:02 +0100

    Create: boardMaker class

---
+++ snakes-and-ladders/src/main/java/BoardMaker.java
@@ -0,0 +1,21 @@
+public class BoardMaker {
+
+    private BoardMaker() {}
+
+    public  static Board createBoard(int size) {
+        if (100 < size || size < 0) {
+          throw new IllegalArgumentException("Invalid board size.");
+        }
+        Board board = new Board(size);
+        for (int i = 0; i < size; i++) {
+          board.addTile(new Tile(i));
+        }
+    
+        for (int i = 0; i < size - 1; i++) {
+          Tile currentTile = board.getTile(i);
+          Tile nextTile = board.getTile(i + 1);
+          currentTile.setNextTile(nextTile);
+        }
+        return board;
+    }
+}
