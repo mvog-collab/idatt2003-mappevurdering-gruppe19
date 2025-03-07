41ea63db70ab7ba6cca6c24bef35df76382fd203
Martha Luise Vogel <marthavogel04@gmail.com>
2025-03-07 13:55:35 +0100

    Create: Start page file

--- snakes-and-ladders/src/main/java/BoardPage.java
+++ snakes-and-ladders/src/main/java/BoardPage.java
@@ -5,11 +5,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class JavaFXApp extends Application {
+public class BoardPage extends Application {
     private final int tileSize = 50;
     private final int width = 10;
     private final int height = 10;
@@ -66,9 +65,6 @@ public class JavaFXApp extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-
-
 
 
     public static void main(String[] args) {
