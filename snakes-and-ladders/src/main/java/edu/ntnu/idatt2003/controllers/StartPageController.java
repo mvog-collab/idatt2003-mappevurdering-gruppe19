0c04c7449ce0e44671031521be2ff811e83b498c
Edvard Klavenes <edvardklavenes@gmail.com>
2025-03-23 16:34:48 +0100

    Feat: create startpage controller

---
+++ snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/controllers/StartPageController.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idatt2003.controllers;
+
+import edu.ntnu.idatt2003.ui.StartPage;
+
+public class StartPageController {
+
+    private final StartPage startPage;
+
+    public StartPageController(StartPage startPage) {
+        this.startPage = startPage;
+    }
+
+    
+}
