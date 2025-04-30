c55049898b96112f11e4d339e9de8e4d7522dda8
Edvard Klavenes <edvardklavenes@gmail.com>
2025-04-30 13:20:58 +0200

    Feat: create logger util and add to methods

---
+++ snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/utils/Log.java
@@ -0,0 +1,9 @@
+package edu.ntnu.idatt2003.utils;
+
+import java.util.logging.Logger;
+
+public final class Log {
+    public static Logger game()   { return Logger.getLogger("GAME"); }
+    public static Logger rules()  { return Logger.getLogger("RULES"); }
+    private Log() {}
+}
