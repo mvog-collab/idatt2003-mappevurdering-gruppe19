277cb4ed99d16bccb4be11b3b5700a8ffb1ffe17
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-02 14:47:38 +0200

    Feat: connect SnlPage to HomePage

--- snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/ui/view/HomePage.java
+++ snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/ui/view/HomePage.java
@@ -1,5 +1,6 @@
 package edu.ntnu.idatt2003.ui.view;
 
+import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,48 +10,77 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-  public class HomePage {
+public class HomePage extends Application {
 
-    public void show(Stage stage) {
-      Label title = new Label("Retro Roll & Rise");
-      title.getStyleClass().add("title");
-
-      // Game buttons (as images)
-      ImageView laddersBtn = new ImageView(new Image(getClass().getResourceAsStream("/images/SnakeAndLadder.png")));
-      laddersBtn.setFitWidth(150);
-      laddersBtn.setFitHeight(150);
-      laddersBtn.setOnMouseClicked((MouseEvent e) -> {
-        System.out.println("Start Stigespill");
-        // TODO: Koble til stigespill
-      });
-
-      laddersBtn.getStyleClass().add("menu-button-image");
-
-
-      ImageView ludoBtn = new ImageView(new Image(getClass().getResourceAsStream("/images/Ludo.png")));
-      ludoBtn.setFitWidth(150);
-      ludoBtn.setFitHeight(150);
-      ludoBtn.setOnMouseClicked((MouseEvent e) -> {
-        System.out.println("Start Ludo");
-        // TODO: Koble til ludo
-      });
-
-      ludoBtn.getStyleClass().add("menu-button-image");
-
-      HBox buttonsBox = new HBox(40, laddersBtn, ludoBtn);
-      buttonsBox.setAlignment(Pos.CENTER);
-
-
-      VBox root = new VBox(40, title, buttonsBox);
-      root.setAlignment(Pos.CENTER);
-      root.setPadding(new Insets(40));
-
-      Scene scene = new Scene(root, 800, 600);
-      stage.setTitle("Retro Roll & Rise");
-      stage.setScene(scene);
-      stage.show();
-    }
+  @Override
+  public void start(Stage stage) {
+    show(stage);
   }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  public void show(Stage stage) {
+    Label title = new Label("Retro Roll & Rise");
+    title.getStyleClass().add("home-page-title");
+
+    // Stigespill-knapp
+    ImageView laddersBtn = new ImageView(new Image(getClass().getResourceAsStream("/images/SnakeAndLadder.png")));
+    laddersBtn.setFitWidth(250);
+    laddersBtn.setFitHeight(250);
+    laddersBtn.getStyleClass().add("menu-button-image");
+
+    laddersBtn.setOnMouseClicked((MouseEvent e) -> {
+      try {
+        new SnlPage().start(new Stage());
+        ((Stage) ((ImageView) e.getSource()).getScene().getWindow()).close(); // Lukk startvinduet
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+    });
+
+    laddersBtn.setOnMouseEntered(e -> {
+      laddersBtn.setScaleX(1.1);
+      laddersBtn.setScaleY(1.1);
+    });
+    laddersBtn.setOnMouseExited(e -> {
+      laddersBtn.setScaleX(1.0);
+      laddersBtn.setScaleY(1.0);
+    });
+
+    // Ludo-knapp
+    ImageView ludoBtn = new ImageView(new Image(getClass().getResourceAsStream("/images/Ludo.png")));
+    ludoBtn.setFitWidth(250);
+    ludoBtn.setFitHeight(250);
+    ludoBtn.getStyleClass().add("menu-button-image");
+    ludoBtn.setOnMouseClicked((MouseEvent e) -> {
+      // TODO: Koble til Ludo
+    });
+    ludoBtn.setOnMouseEntered(e -> {
+      ludoBtn.setScaleX(1.1);
+      ludoBtn.setScaleY(1.1);
+    });
+    ludoBtn.setOnMouseExited(e -> {
+      ludoBtn.setScaleX(1.0);
+      ludoBtn.setScaleY(1.0);
+    });
+
+    HBox buttonBox = new HBox(40, laddersBtn, ludoBtn);
+    buttonBox.setAlignment(Pos.CENTER);
+
+    VBox root = new VBox(40, title, buttonBox);
+    root.setAlignment(Pos.CENTER);
+    root.setPadding(new Insets(40));
+    root.getStyleClass().add("page-background");
+
+    Scene scene = new Scene(root, 800, 600);
+    scene.getStylesheets().add(getClass().getResource("/styles/style.css").toExternalForm());
+
+    stage.setTitle("Retro Roll & Rise");
+    stage.setScene(scene);
+    stage.show();
+  }
+}
