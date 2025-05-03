ddcbae6de2847f6e6e4f8d66b2aaaefeee78881c
Edvard Klavenes <edvardklavenes@gmail.com>
2025-05-03 10:02:47 +0200

    Refactor: bump function to not bump on null

--- snakes-and-ladders/src/main/java/edu/games/engine/board/MovementPath.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/board/MovementPath.java
@@ -1,7 +1,9 @@
 package edu.games.engine.board;
 
+import edu.games.engine.model.LudoColor;
+
 public interface MovementPath {
     Tile start();
-    Tile next(Tile current, int steps);
+    Tile next(Tile current, int steps, LudoColor owner);
     boolean isEnd(Tile tile);
 }
