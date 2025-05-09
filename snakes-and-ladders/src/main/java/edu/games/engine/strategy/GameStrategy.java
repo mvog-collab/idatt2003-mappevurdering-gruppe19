3ebcd6f1d77d92e999f2c089fb5a672bab92393c
Edvard Klavenes <edvardklavenes@gmail.com>
2025-05-09 12:36:49 +0200

    Refactor: move strategy out of gateway and separate conserns

---
+++ snakes-and-ladders/src/main/java/edu/games/engine/strategy/GameStrategy.java
@@ -0,0 +1,14 @@
+package edu.games.engine.strategy;
+
+import edu.games.engine.board.Tile;
+import edu.games.engine.impl.DefaultGame;
+import edu.games.engine.model.Player;
+import edu.games.engine.model.PlayerPiece;
+
+public interface GameStrategy {
+    void initializeGame(DefaultGame game);
+    boolean processDiceRoll(Player player, int diceValue, DefaultGame game);
+    Tile movePiece(Player player, int pieceIndex, int diceValue, DefaultGame game);
+    boolean checkWinCondition(Player player, DefaultGame game);
+    void applySpecialRules(Player player, PlayerPiece piece, Tile destinationTile, DefaultGame game);
+}
