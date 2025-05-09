48cd12a71973a53ab13f1dfbbcf8255c51e884f2
Edvard Klavenes <edvardklavenes@gmail.com>
2025-05-09 12:36:15 +0200

    Feat: add observer implementation for further use

---
+++ snakes-and-ladders/src/main/java/edu/games/engine/observer/BoardGameEvent.java
@@ -0,0 +1,31 @@
+package edu.games.engine.observer;
+
+public class BoardGameEvent {
+    public enum EventType {
+        PLAYER_MOVED,
+        DICE_ROLLED,
+        GAME_STARTED,
+        GAME_RESET,
+        WINNER_DECLARED,
+        PLAYER_ADDED,
+        TURN_CHANGED,
+        PLAYERS_LOADED,
+        PIECE_SELECTED
+    }
+    
+    private final EventType type;
+    private final Object data;
+    
+    public BoardGameEvent(EventType type, Object data) {
+        this.type = type;
+        this.data = data;
+    }
+    
+    public EventType getType() {
+        return type;
+    }
+    
+    public Object getData() {
+        return data;
+    }
+}
