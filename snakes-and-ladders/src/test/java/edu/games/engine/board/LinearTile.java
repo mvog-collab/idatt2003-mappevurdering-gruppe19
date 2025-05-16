c7b828be648d176bf24b4f79d43f7d03100117ac
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-16 19:45:00 +0200

    Test: test the linear tile class

--- snakes-and-ladders/src/test/java/edu/games/engine/board/LinearTile.java
+++ snakes-and-ladders/src/test/java/edu/games/engine/board/LinearTile.java
@@ -1,5 +1,28 @@
 package edu.games.engine.board;
 
-public class LinearTile {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinearTileTest {
+
+  @Test
+  void shouldReturnCorrectId() {
+    LinearTile tile = new LinearTile(7);
+    assertEquals(7, tile.id());
+  }
+
+  @Test
+  void shouldReturnNullNextIfNotConnected() {
+    LinearTile tile = new LinearTile(1);
+    assertNull(tile.next());
+  }
+
+  @Test
+  void shouldReturnNextTileIfConnected() {
+    LinearTile first = new LinearTile(1);
+    LinearTile second = new LinearTile(2);
+    first.next = second;
+    assertEquals(second, first.next());
+  }
 }
