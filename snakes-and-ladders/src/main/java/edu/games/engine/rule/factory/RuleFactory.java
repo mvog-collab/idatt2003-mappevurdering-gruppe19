b99c1231eff286a5654655476416a66e86c60587
Edvard Klavenes <edvardklavenes@gmail.com>
2025-05-19 11:59:57 +0200

    Refactor: remove unused config

--- snakes-and-ladders/src/main/java/edu/games/engine/rule/factory/RuleFactory.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/rule/factory/RuleFactory.java
@@ -1,9 +1,8 @@
 package edu.games.engine.rule.factory;
 
-import edu.games.engine.rule.RuleConfig;
 import edu.games.engine.rule.RuleEngine;
 import edu.ntnu.idatt2003.persistence.BoardAdapter;
 
 public interface RuleFactory {
-  RuleEngine create(BoardAdapter.MapData data, RuleConfig config);
+  RuleEngine create(BoardAdapter.MapData data);
 }
