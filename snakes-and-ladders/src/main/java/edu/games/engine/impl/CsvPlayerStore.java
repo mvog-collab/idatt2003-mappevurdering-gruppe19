c9ba720a40bde491325cfda47c18681163dbf22e
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-21 19:16:44 +0200

    Refactor: change CsvPlayer from throwing IOExeptions to StorageException

--- snakes-and-ladders/src/main/java/edu/games/engine/impl/CsvPlayerStore.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/impl/CsvPlayerStore.java
@@ -1,10 +1,12 @@
 package edu.games.engine.impl;
 
 import edu.games.engine.exception.GameEngineException;
+import edu.games.engine.exception.StorageException;
 import edu.games.engine.model.Player;
 import edu.games.engine.model.Token;
 import edu.games.engine.store.PlayerStore;
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -15,27 +17,29 @@ public final class CsvPlayerStore implements PlayerStore {
   private static final String SEP = ",";
 
   @Override
-  public void save(List<Player> players, Path out) throws IOException {
+  public void save(List<Player> players, Path out) throws StorageException {
     try (BufferedWriter w = new BufferedWriter(new FileWriter(out.toFile()))) {
       for (Player p : players) {
         w.write("%s%s%s%s%s%n".formatted(
             p.getName(), SEP, p.getToken(), SEP, p.getBirtday()));
       }
     } catch (IOException e) {
-      throw new GameEngineException("Could not save players to " + out, e);
+      throw new StorageException("Could not save players to " + out, e);
     }
   }
 
   @Override
-  public List<Player> load(Path in) throws IOException {
+  public List<Player> load(Path in) throws StorageException {
+    try (BufferedReader r = Files.newBufferedReader(in)) {
     List<Player> list = new ArrayList<>();
-    try (BufferedReader r = new BufferedReader(new FileReader(in.toFile()))) {
       String line;
       while ((line = r.readLine()) != null) {
         String[] parts = line.split(SEP);
         list.add(new Player(parts[0], Token.valueOf(parts[1]), LocalDate.parse(parts[2])));
       }
+      return list;
+    } catch (IOException | StorageException e) {
+      throw new StorageException("Could not load players from " + in, e);
     }
-    return list;
   }
 }
