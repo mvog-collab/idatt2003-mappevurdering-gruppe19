e684819f4905b1564876f61b2f41f065ae6d2f1d
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-21 18:54:16 +0200

    Feat: add validation exception class

--- snakes-and-ladders/src/main/java/edu/games/engine/exception/ValidationException.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/exception/ValidationException.java
@@ -1,5 +1,12 @@
 package edu.games.engine.exception;
 
-public class ValidationException {
+public class ValidationException extends GameEngineException {
 
+  public ValidationException(String message) {
+    super(message);
+  }
+
+  public ValidationException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
