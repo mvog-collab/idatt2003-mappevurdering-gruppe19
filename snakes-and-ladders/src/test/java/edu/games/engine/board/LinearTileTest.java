cc41ce42f1cbdad5568fcf14f23d900a661c7937
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-22 21:14:53 +0200

    Test: add tests for LinearTile class

--- snakes-and-ladders/src/test/java/edu/games/engine/board/LinearTileTest.java
+++ snakes-and-ladders/src/test/java/edu/games/engine/board/LinearTileTest.java
@@ -9,20 +9,30 @@ class LinearTileTest {
   @Test
   void shouldReturnCorrectTileId() {
     LinearTile tile = new LinearTile(7);
-    assertEquals(7, tile.tileId());
+    assertEquals(7, tile.tileId(), "Tile ID should match the constructor value");
   }
 
   @Test
-  void shouldReturnNullNextIfNotConnected() {
+  void shouldReturnNullAsNextIfNotSet() {
     LinearTile tile = new LinearTile(1);
-    assertNull(tile.next());
+    assertNull(tile.next(), "Next should be null if no next tile is set");
   }
 
   @Test
   void shouldReturnNextTileIfConnected() {
     LinearTile first = new LinearTile(1);
     LinearTile second = new LinearTile(2);
+
     first.nextTile = second;
-    assertEquals(second, first.next());
+
+    assertEquals(second, first.next(), "Next should return the connected tile");
+    assertEquals(2, first.next().tileId(), "Next tile ID should be correct");
+  }
+
+  @Test
+  void nextTileShouldBeMutableWithinPackage() {
+    LinearTile tile = new LinearTile(10);
+    assertDoesNotThrow(() -> tile.nextTile = new LinearTile(11));
+    assertEquals(11, tile.next().tileId());
   }
 }
