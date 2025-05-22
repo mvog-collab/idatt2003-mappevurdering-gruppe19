0142d5a9bf48935fb6f78b4a44140f66fdc43ef9
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-22 22:36:38 +0200

    Feat: add javadoc to JsonBoardLoader class

--- snakes-and-ladders/src/main/java/edu/games/engine/board/factory/JsonBoardLoader.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/board/factory/JsonBoardLoader.java
@@ -2,6 +2,18 @@ package edu.games.engine.board.factory;
 
 import edu.games.engine.board.Board;
 
+/**
+ * Interface for loading or creating a {@link Board} instance, typically from structured data like JSON.
+ * <p>
+ * Implementations define how a board of a given size should be created.
+ */
 public interface JsonBoardLoader {
+
+  /**
+   * Creates a new {@link Board} instance with the specified size.
+   *
+   * @param size the desired size or number of tiles on the board
+   * @return a new {@link Board} instance
+   */
   Board create(int size);
 }
