8a44084b150e99f1f02463c2a3a74f25f0559bd6
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-22 22:57:35 +0200

    Feat: add javadoc to DiceFactory interface

--- snakes-and-ladders/src/main/java/edu/games/engine/dice/factory/DiceFactory.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/dice/factory/DiceFactory.java
@@ -2,6 +2,15 @@ package edu.games.engine.dice.factory;
 
 import edu.games.engine.dice.Dice;
 
+/**
+ * Factory interface for creating {@link Dice} instances.
+ */
 public interface DiceFactory {
+
+  /**
+   * Creates a new dice instance.
+   *
+   * @return a new {@code Dice} object
+   */
   Dice create();
 }
