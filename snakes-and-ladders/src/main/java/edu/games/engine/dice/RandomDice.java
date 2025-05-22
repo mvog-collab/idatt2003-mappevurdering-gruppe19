15dd623da4b2eca6f60e50d224854b02ae1281a1
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-22 23:02:44 +0200

    Feat: add javadoc to RandomDice class

--- snakes-and-ladders/src/main/java/edu/games/engine/dice/RandomDice.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/dice/RandomDice.java
@@ -5,20 +5,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * A dice implementation that rolls one or more six-sided dice using {@link Random}.
+ */
 public class RandomDice implements Dice {
 
   private final int numberOfDice;
   private final Random random;
   private final List<Integer> lastRolledValues = new ArrayList<>();
 
+  /**
+   * Creates a new random dice roller with a given number of dice.
+   *
+   * @param numberOfDice the number of dice to roll
+   * @throws ValidationException if the number is less than 1
+   */
   public RandomDice(int numberOfDice) {
-    if (numberOfDice < 1 ) {
+    if (numberOfDice < 1) {
       throw new ValidationException("Invalid number of dice: must be greater than 0");
     }
-    this.numberOfDice= numberOfDice;
+    this.numberOfDice = numberOfDice;
     this.random = new Random();
   }
 
+  /**
+   * Creates a new random dice roller with a given number of dice and a custom random source.
+   *
+   * @param numberOfDice the number of dice to roll
+   * @param rnd a {@link Random} instance to use
+   * @throws ValidationException if arguments are invalid
+   */
   public RandomDice(int numberOfDice, Random rnd) {
     if (numberOfDice < 1)
       throw new ValidationException("Invalid number of dice: must be greater than 0");
@@ -29,6 +45,11 @@ public class RandomDice implements Dice {
     this.random = rnd;
   }
 
+  /**
+   * Rolls all dice and returns the total value.
+   *
+   * @return the sum of the dice rolled
+   */
   @Override
   public int roll() {
     lastRolledValues.clear();
@@ -41,11 +62,20 @@ public class RandomDice implements Dice {
     return sum;
   }
 
+  /**
+   * Returns how many dice this instance rolls.
+   *
+   * @return the number of dice
+   */
   public int getDiceCount() {
     return numberOfDice;
   }
 
-
+  /**
+   * Gets the values from the last roll.
+   *
+   * @return an unmodifiable list of last rolled values
+   */
   @Override
   public List<Integer> lastValues() {
     return List.copyOf(lastRolledValues);
