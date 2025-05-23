afb4d1f2c8444e2786b2b4a88471d927528f0e44
Martha Luise Vogel <marthavogel04@gmail.com>
2025-05-23 04:45:30 +0200

    Feat: add javadoc to exception classes

--- snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/exception/GameInitializationException.java
+++ snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/exception/GameInitializationException.java
@@ -1,10 +1,27 @@
 package edu.ntnu.idatt2003.exception;
 
+/**
+ * Signals a failure during the initialization of a game session,
+ * such as errors setting up board, players, or resources.
+ */
 public class GameInitializationException extends ApplicationException {
+
+    /**
+     * Constructs a new GameInitializationException with the specified detail message.
+     *
+     * @param message a description of the initialization failure
+     */
     public GameInitializationException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs a new GameInitializationException with the specified detail message
+     * and underlying cause.
+     *
+     * @param message a description of the initialization failure
+     * @param cause   the root exception that triggered this failure
+     */
     public GameInitializationException(String message, Throwable cause) {
         super(message, cause);
     }
--- snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/exception/JsonParsingException.java
+++ snakes-and-ladders/src/main/java/edu/ntnu/idatt2003/exception/JsonParsingException.java
@@ -1,6 +1,18 @@
 package edu.ntnu.idatt2003.exception;
 
+/**
+ * Indicates an error occurred while parsing JSON content,
+ * such as invalid syntax or missing fields.
+ */
 public class JsonParsingException extends ApplicationException {
+
+    /**
+     * Constructs a new JsonParsingException with the specified detail message
+     * and underlying cause.
+     *
+     * @param message a description of the JSON parsing error
+     * @param cause   the root exception thrown during parsing
+     */
     public JsonParsingException(String message, Throwable cause) {
         super(message, cause);
     }
