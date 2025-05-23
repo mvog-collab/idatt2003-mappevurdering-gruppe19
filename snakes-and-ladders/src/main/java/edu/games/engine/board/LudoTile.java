56949bfc989cb125bb416413e84057634272e073
Edvard Klavenes <edvardklavenes@gmail.com>
2025-05-23 03:27:16 +0200

    Chore: fix javadoc

--- snakes-and-ladders/src/main/java/edu/games/engine/board/LudoTile.java
+++ snakes-and-ladders/src/main/java/edu/games/engine/board/LudoTile.java
@@ -3,12 +3,15 @@ package edu.games.engine.board;
 import edu.games.engine.model.LudoColor;
 
 /**
- * Returns the next tile for the given player color.
- * This may differ depending on whether the player is entering the goal path.
- *
- * @param player the player's color
- * @return the next tile in the path
+ * Represents a single position or square on the a Ludo board.
  */
 public interface LudoTile extends Tile {
+  /**
+   * Returns the next tile for the given player color.
+   * This may differ depending on whether the player is entering the goal path.
+   *
+   * @param player the player's color
+   * @return the next tile in the path
+   */
   LudoTile next(LudoColor player);
 }
